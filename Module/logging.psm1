68280fbe04286e307693ba01617652015166be70
miwes <weis@outlook.cz>
2016-12-28 20:13:23 +0100

    Version 1.0

--- Module/logging.psm1
+++ Module/logging.psm1
@@ -1,7 +1,12 @@
 <#
 .SYNOPSIS
-Module urceny pro logovani do souboru, konzole
+Module urceny pro logovani do souboru CSV, konzole
 #>
+
+Set-StrictMode -Version Latest
+
+[Threading.Thread]::CurrentThread.CurrentCulture = 'en-US'
+[String]$CSVHeader = 'Time;Type;Message'
 
 <#
 .SYNOPSIS
@@ -11,6 +16,31 @@ Function Private_GetDate {
     [string]$Date = $(Get-Date -Format('yyyy-MM-dd HH:mm:ss'))
     Return $Date
 }
+
+<#
+.SYNOPSIS
+Funkce kontroluje existenci log file, pokud neexistuje zaloziho a prida na zacatek hlavicku
+.Parameter LogFile
+Soubor kam se maji informace zaspat. Data jsou vzdy pridany
+.Parameter CSVHeader
+Hlavicka CSV souboru
+#>
+Function Private_SetLogFile {
+    [CmdletBinding()]Param(
+         [Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+         [string]$LogFile
+        
+         ,[Parameter(Mandatory=$true,ValueFromPipeline=$True)]
+         [string]$CSVHeader
+    )
+    If (![System.IO.File]::Exists($LogFile)) {
+        $stream = New-Object 'System.IO.StreamWriter' -ArgumentList $logFile, $False
+        $stream.WriteLine($CSVHeader)
+        $stream.Close()
+    }
+    Return 0
+}
+
 
 <#
 .SYNOPSIS
@@ -36,14 +66,28 @@ Function Add-Log {
      )
     BEGIN {
         $Date = Private_GetDate
-        $LogMessage = $Date + ';' + $type + ';' + $Message
+        $LogMessageCSV = $Date + ';' + $type + ';' + $Message
+        $LogMessageConsole = $Date + ' -> [' + $type + '] ' + $Message
     }
 
     PROCESS {
         If($logFile) {
-            $message | out-file $logFile -Append
+            $ReturnValue = Private_SetLogFile -logFile $logFile -CSVHeader $CSVHeader    
+            If($ReturnValue -eq -1) {
+                Write-Verbose "Cannot create log file ! - $logFile"
+                Return -1
+            }
+            Try {
+                $stream = New-Object 'System.IO.StreamWriter' -ArgumentList $logFile, $True
+                $stream.WriteLine($LogMessageCSV)
+                $stream.Close()
+            }
+            Catch {
+                Write-Verbose "Cannot append to log file ! - $logFile"
+                Return -1
+            }
         }
-        Write-Verbose $LogMessage
+        Write-Verbose $LogMessageConsole
     }
 
     END {
