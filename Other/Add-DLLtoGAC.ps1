de89ff02b872098868cca61e05d3de6654b11271
miwes <weis@outlook.cz>
2017-09-06 20:44:01 +0200

    Add DLL toGAC
    Add DLL toGAC

---
+++ Other/Add-DLLtoGAC.ps1
@@ -0,0 +1,4 @@
+#Note that you should be running PowerShell as an Administrator
+[System.Reflection.Assembly]::Load("System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")            
+$publish = New-Object System.EnterpriseServices.Internal.Publish            
+$publish.GacInstall("C:\Path\To\DLL.dll")
