bf276b723a71b883d1b4d184fb7550f2f8390091
MiWes <weis@outlook.cz>
2022-01-20 07:48:36 +0100

    PSSession

---
+++ Other/set-windowDrag.ps1
@@ -0,0 +1,18 @@
+﻿$CSharpSig = @'
+
+[DllImport("user32.dll", EntryPoint = "SystemParametersInfo")]
+
+public static extern bool SystemParametersInfo(
+
+                 uint uiAction,
+
+                 uint uiParam,
+
+                 uint pvParam,
+
+                 uint fWinIni);
+
+'@
+$SetWindowAPI = Add-Type -MemberDefinition $CSharpSig -Name WinAPICall -Namespace SystemParamInfo –PassThru
+# disable Window drag
+$SetWindowAPI::SystemParametersInfo(0x0025, 0, $null,0)
