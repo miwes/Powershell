00ad489a45d3f8ad58332633620d850505b2bf61
MiWes <weis@outlook.cz>
2022-04-28 14:44:33 +0200

    Update get-ServiceAccount.ps1

--- Reports/get-ServiceAccount.ps1
+++ Reports/get-ServiceAccount.ps1
@@ -2,7 +2,7 @@ $servers = Get-ADComputer -Filter * -SearchBase "OU=Servery,DC=ad,DC=cz" -Search
 $services = ForEach ($server In $servers) {
     Write-Host $server.name
     Try {
-        Get-WmiObject win32_service -ComputerName $server.name | Where-Object {$_.startname -like "kovo*"} | Select-Object PSComputerName, Name, Startname
+        Get-WmiObject win32_service -ComputerName $server.name | Where-Object {$_.startname -like "*kovo*"} | Select-Object PSComputerName, Name, Startname
     } Catch {
         Write-Host $Error[0]
     }
