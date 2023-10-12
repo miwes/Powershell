7319aab73f5fc2c42d598b7912d10b5551d34cba
MiWes <weis@outlook.cz>
2023-10-12 08:11:52 +0200

    Update Set-DNSRemote.ps1

--- DNS/Set-DNSRemote.ps1
+++ DNS/Set-DNSRemote.ps1
@@ -2,12 +2,12 @@
     [Parameter(Mandatory=$True,Position=1)] [Alias("Server")] [string]$attrServer
 )
 
-$oldDNS = "192.168.169.12"
-$newDNS = ("172.16.10.11", "172.16.138.11")
+$oldDNS = ("172.30.100.11", "172.30.100.12")
+$newDNS = ("172.30.100.11", "172.30.120.12")
 
 Try {
-    $Session = New-CimSession -ComputerName $attrServer #-Authentication Kerberos
-    Get-DnsClientServerAddress -CimSession $Session | Where-Object {$_.AddressFamily -eq 2 -and [system.String]::Join(",",$_.ServerAddresses) -eq $oldDNs} | Set-DnsClientServerAddress -ServerAddresses $newDNS -CimSession $Session
+    $Session = New-CimSession -ComputerName $attrServer
+    Get-DnsClientServerAddress -CimSession $Session | Where-Object {$_.AddressFamily -eq 2 -and [system.String]::Join(",",$_.ServerAddresses) -eq [system.String]::Join(",",$oldDNS)} | Set-DnsClientServerAddress -ServerAddresses $newDNS -CimSession $Session
     Get-DnsClientServerAddress -CimSession $Session | Where-Object {$_.AddressFamily -eq 2 }
 } Catch {
     Write-Warning "Error = $($Error[0])"
